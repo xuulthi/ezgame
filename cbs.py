ee8ff11d5be1b0a8f974f7f00eabfc220bdf0659
10xcode <45268507+xuulthi@users.noreply.github.com>
2019-10-31 23:07:16 +0200

    Add files via upload
    Cows and Bulls game

---
+++ cbs.py
@@ -0,0 +1,20 @@
+import random as r
+
+r.seed();
+cows=0;
+bulls=0;
+number = str(r.randint(1000,9999));
+
+while True:
+    guess=input("Poznai")
+    for cifra in guess:
+        if cifra is number:
+            if number.index(cifra) == guess.index(cifra):
+                bulls+=1;
+            else:
+                cows+=1;
+        if bulls==4:
+            print("You win.GG")
+            break
+        bulls=0;
+        cows=0;
